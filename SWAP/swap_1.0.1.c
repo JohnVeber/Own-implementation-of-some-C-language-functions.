1a6e1f95ed6880108997f02f8bff8894ed44428e
John Veber <evgeniy.weber@gmail.com>
2024-04-25 10:41:11 +0300

    swap_1.0.1 complete.

---
+++ SWAP/swap_1.0.1.c
@@ -0,0 +1,27 @@
+/* My_swap_function_1.0.1.c*/
+/* This program is the second attempt to realise swap function in C */
+/* To achieve the goal of this programm pointers are used */
+
+#include <stdio.h>
+
+void swap_i (int *pc, int *pd); // Function declaration
+
+int main (void)
+{
+    int a = 21;
+    int b = 17;
+    printf("Values before swapping were: a = %d, b = %d.\n", a, b);
+    swap_i(&a, &b); // Function call
+    printf("Values after swapping are: ");
+    printf("a = %d, b = %d\n\n\n", a, b);
+    return 0;
+}
+void swap_i(int *pc, int *pd) // Function definition
+{
+    int t = *pc;
+    *pc = *pd;
+    *pd = t;
+    return;
+}
+
+/* The function still works for integers only at this version of program */
